d7edb7bf26182f6c04b7646e5698f5c2ae3673fa
maurorsc <90015755+maurorsc@users.noreply.github.com>
2022-12-11 17:24:47 -0300

    Add files via upload

---
+++ Proyectomauro2.js
@@ -0,0 +1,11 @@
+const newLocal = 3;
+
+    const ListaMisDatos = ["Mauro", 28, true, november 3 1994];
+    const LibroFav = ["EL PODER DE LAS PALABRAS", "Sigman Mariano", 2022, https://www.penguinlibros.com/ar/ciencia-y-tecnologia/310899-libro-el-poder-de-las-palabras-9789877950465];
+    
+
+
+
+console.log(ListaMisDatos); 
+
+
